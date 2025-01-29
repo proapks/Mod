3a8444bb3d4e1a7acf8f235e5a62bcaaafce701f
ProApk <ipopcornmovie@gmail.com>
2025-01-29 09:24:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("downloadBtn").addEventListener("click", function() {
+    window.location.href = "https://yourdirectapilink.com/app.apk"; // Yahan APK ka direct link daalo
+});
